30cc71368241bbc42fd5c1dca87b8613e44ca729
qianlv <qianlv7@qq.com>
2015-04-22 14:41:15 +0800

    添加异常处理，日志

--- question.py
+++ question.py
@@ -23,10 +23,10 @@ class Question(ZhiHuPage):
         else:
             try:
                 title_div = self.soup.find("div", id="zh-question-title")
-            except AttributeError, e:
-                print e
+                title = title_div.get_text()
+            except Exception, e:
+                logging.warn("Question get_title error|%s|%s", self.url, str(e))
                 return None
-            title = title_div.get_text()
             title = remove_blank_lines(title)
             self.title = title
         return self.title.encode("utf-8")
@@ -35,11 +35,11 @@ class Question(ZhiHuPage):
         detail = None
         try:
             detail_div = self.soup.find("div", id="zh-question-detail").div
-        except AttributeError, e:
-            print e
+            detail = detail_div.get_text()
+        except Exception, e:
+            logging.warn("Question get_detail error|%s|%s", self.url, str(e))
             return None
 
-        detail = detail_div.get_text()
         detail = remove_blank_lines(detail)
         return detail.encode("utf-8")
 
@@ -55,40 +55,47 @@ class Question(ZhiHuPage):
                     self.answers_num = len(soup)
                 else:
                     self.answers_num = get_number_from_string(unicode(soup.string))[0]
-            except (AttributeError, IndexError), e:
-                print e
+            except Exception, e:
+                logging.warn("Question get_answers_num error|%s|%s", self.url, str(e))
                 return None
+
         return self.answers_num
 
     def get_answers(self):
-        soup = self.soup.find("div", id="zh-question-answer-wrap")
-        answer_link = soup.find_all("a", class_="answer-date-link")
-        for link in answer_link:
-            url = ZHI_HU_URL + link.get("href")
-            print url
-            yield answer.Answer(url)
+        try:
+            soup = self.soup.find("div", id="zh-question-answer-wrap")
+            answer_link = soup.find_all("a", class_="answer-date-link")
+            for link in answer_link:
+                url = ZHI_HU_URL + link.get("href")
+                yield answer.Answer(url)
+        except Exception, e:
+            logging.warn("Question get_answers error|%s|%s", self.url, str(e))
+            return
 
         answer_num = (self.get_answers_num() + 49) / 50
         if answer_num > 1:
-            data_init = json.loads(soup.get("data-init"))
-            post_url = ZHI_HU_URL + "/node/" + data_init['nodename'] 
-            input_tag = self.soup.find("input", attrs={"type":"hidden", "name": "_xsrf"})
-            value = input_tag.get("value")
-            for offset in range(50, answer_num * 50 + 50, 50):
-                data_init['params']['offset'] = offset
-                data = {
-                    "_xsrf": value,
-                    "method": "next",
-                    "params": json.dumps(data_init["params"])
-                    }
-                response = self.get_post(post_url, data) 
-                for content in response.json()['msg']:
-                    soup = BeautifulSoup(content)
-                    link = soup.find("a", 
-                                class_="answer-date-link")
-                    url = ZHI_HU_URL + link.get("href")
-                    print url
-                    yield answer.Answer(url)
+            try:
+                data_init = json.loads(soup.get("data-init"))
+                post_url = ZHI_HU_URL + "/node/" + data_init['nodename'] 
+                input_tag = self.soup.find("input", attrs={"type":"hidden", "name": "_xsrf"})
+                value = input_tag.get("value")
+                for offset in range(50, answer_num * 50 + 50, 50):
+                    data_init['params']['offset'] = offset
+                    data = {
+                        "_xsrf": value,
+                        "method": "next",
+                        "params": json.dumps(data_init["params"])
+                        }
+                    response = self.get_post(post_url, data) 
+                    for content in response.json()['msg']:
+                        soup = BeautifulSoup(content)
+                        link = soup.find("a", 
+                                    class_="answer-date-link")
+                        url = ZHI_HU_URL + link.get("href")
+                        yield answer.Answer(url)
+            except Exception, e:
+                logging.warn("Question get_answers error|%s|%s", self.url, str(e))
+                return
         
 
     def get_follower_num(self): 
@@ -97,10 +104,10 @@ class Question(ZhiHuPage):
         else:
             try:
                 soup = self.soup.find("div", id="zh-question-side-header-wrap")
-            except AttributeError, e:
-                print e
+                self.follower_num = get_number_from_string(soup.get_text())[0]
+            except Exception, e:
+                logging.warn("Question get_follower_num error|%s|%s", self.url, str(e))
                 return None
-            self.follower_num = get_number_from_string(soup.get_text())[0]
         return self.follower_num
 
     def get_topics(self):
@@ -110,8 +117,8 @@ class Question(ZhiHuPage):
             for topic_tag in topic_all:
                 url = ZHI_HU_URL + topic_tag.get('href')
                 yield topic.Topic(url)
-        except AttributeError, e:
-            print e
+        except Exception, e:
+            logging.warn("Question get_topics error|%s|%s", self.url, str(e))
             return
 
 
