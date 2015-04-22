5fae9122fad398fa2b11b93f4cdf4fd79b34916f
qianlv <qianlv7@qq.com>
2015-04-22 16:02:47 +0800

    异常处理，日志，mysql数据库

--- zhihuBase.py
+++ zhihuBase.py
@@ -7,6 +7,9 @@ sys.setdefaultencoding('utf-8')
 import json
 import os
 import re
+import logging
+
+from ConfigParser import SafeConfigParser
 
 # 常量
 ZHI_HU_URL = "http://www.zhihu.com"
@@ -37,6 +40,7 @@ def is_num_by_except(num):
         return False
 
 import requests
+logging.getLogger("requests").setLevel(logging.WARNING)
 from bs4 import BeautifulSoup
 
 session = None
@@ -58,8 +62,9 @@ def login(email, passwd):
     if response.json()['r'] == 1:
         print 'Login Failed, reason is:'
         for m in response.json()['msg']:
+            logging.error("Login Failed, reason is: %s", \
+                response.json()['msg'][m].encode("utf-8"))
             exit(response.json()['msg'][m].encode("utf-8"))
-            
 
     session = s
 
@@ -75,10 +80,10 @@ class ZhiHuPage(object):
 
     def __get_session(self):
         global session
-        from ConfigParser import SafeConfigParser
         try:
             f = open("config.ini")
-        except IOError:
+        except IOError, e:
+            logging.error("You must be set config.ini|%d|%s", os.getpid(), str(e))
             exit("You must be set config.ini")
         config = SafeConfigParser()
         config.readfp(f)
@@ -101,21 +106,25 @@ class ZhiHuPage(object):
 
     def get_id(self):
         if self.url:
-            return self.url.split("/")[4]
+            return int(self.url.split("/")[4])
 
     def get_page(self, url):
         global session
         self.__get_session()
         try:
             response = session.get(url)
+            if (response.status_code != 200):
+                logging.warn("Can't get right webpage|%s|%d", url, response.status_code)
+                return None
             soup = BeautifulSoup(response.content)
-        except requests.ConnectionError:
-            print 'Network Problem'
-            soup = None
-        except requests.Timeout:
-            print 'Request times out'
-            soup = None
-        return soup
+            return soup
+        except requests.ConnectionError, e:
+            logging.error("Network Problem: %s", str(e))
+        except requests.Timeout, e:
+            logging.error("Time out: %s", str(e))
+        except Exception, e:
+            logging.error("Session Post Fail: %s", str(e))
+        return None
 
     def get_post(self, url, data):
         global session
@@ -128,16 +137,63 @@ class ZhiHuPage(object):
                 'Referer': url,
             }
             response = session.post(url, data=data, headers=headers)
+            if (response.status_code != 200):
+                logging.warn("Can't get right webpage|%s|%d", url, response.status_code)
+                return None
             return response 
-        except requests.ConnectionError:
-            print 'Network Problem'
-        except requests.Timeout:
-            print 'Request times out'
+        except requests.ConnectionError, e:
+            logging.error("Network Problem: %s", str(e))
+        except requests.Timeout, e:
+            logging.error("Time out: %s", str(e))
+        except Exception, e:
+            logging.error("Session Post Fail: %s", str(e))
         return None
 
+import MySQLdb
 
+class CrawlerDb(object):
+    def __init__(self):
+        try:
+            f = open("config.ini")
+        except IOError, e:
+            logging.error("You must be set config.ini|%d|%s", os.getpid(), str(e))
+            exit("You must be set config.ini")
+        config = SafeConfigParser()
+        config.readfp(f)
+        dbname   = config.get("mysql", "dbname")
+        username = config.get("mysql", "username")
+        passwd   = config.get("mysql", "passwd")
+        print dbname, username, passwd
+        self.db = MySQLdb.connect("localhost", username, passwd, dbname, charset="utf8")
+    
+    def __del__(self):
+        self.db.close()
 
+    def dbcommit(self):
+        self.db.commit()
 
+    def dbrollback(self):
+        self.db.rollback()
+
+    def dbexecute(self, sql):
+        try:
+            cursor = self.db.cursor()
+            cursor.execute(sql)
+            self.dbcommit()
+        except Exception, e:
+            logging.error("Execute sql fail|%s|%s", str(e), sql)
+            self.dbrollback()
+    
+    def dbsearch(self, sql):
+        try:
+            cursor = self.db.cursor()
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return result
+        except Exception, e:
+            logging.error("search sql fail|%s|%s", sql, str(e))
+            return None
+            
 
 
 if __name__ == '__main__':
